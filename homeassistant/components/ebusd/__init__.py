228333138dd53c88b9f54d4f196451f6b8a6f8d0
Daniel Jakschik <daniel.jakschik@commsolid.com>
2019-12-17 20:45:40 +0100

    WIP

--- homeassistant/components/ebusd/__init__.py
+++ homeassistant/components/ebusd/__init__.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 import logging
 import socket
 
-import ebusdpy
+import ebus
 import voluptuous as vol
 
 from homeassistant.const import (
@@ -16,40 +16,33 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.util import Throttle
 
-from .const import DOMAIN, SENSOR_TYPES
+from .const import DOMAIN, CONF_CIRCUIT, CONF_CIRCUITMAP, DEFAULT_PORT, DEFAULT_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "ebusd"
-DEFAULT_PORT = 8888
-CONF_CIRCUIT = "circuit"
-CACHE_TTL = 900
-SERVICE_EBUSD_WRITE = "ebusd_write"
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=15)
+# SERVICE_EBUSD_WRITE = "ebusd_write"
 
-
-def verify_ebusd_config(config):
-    """Verify eBusd config."""
-    circuit = config[CONF_CIRCUIT]
-    for condition in config[CONF_MONITORED_CONDITIONS]:
-        if condition not in SENSOR_TYPES[circuit]:
-            raise vol.Invalid(f"Condition '{condition}' not in '{circuit}'.")
-    return config
-
+# MONITOR_SCHEMA = vol.Schema(
+#     {
+#         vol.Required(CONF_KNX_EXPOSE_TYPE): cv.string,
+#         vol.Optional(CONF_ENTITY_ID): cv.entity_id,
+#         vol.Required(CONF_KNX_EXPOSE_ADDRESS): cv.string,
+#     }
+# )
 
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             vol.All(
                 {
-                    vol.Required(CONF_CIRCUIT): cv.string,
                     vol.Required(CONF_HOST): cv.string,
                     vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
                     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+                    vol.Optional(CONF_CIRCUIT): cv.string,
                     vol.Optional(CONF_MONITORED_CONDITIONS, default=[]): cv.ensure_list,
-                },
-                verify_ebusd_config,
+                    # vol.Optional(CONF_CIRCUITMAP, default={}): cv.ensure_list,
+                }
             )
         )
     },
@@ -57,71 +50,77 @@ CONFIG_SCHEMA = vol.Schema(
 )
 
 
-def setup(hass, config):
+async def async_setup(hass, config):
     """Set up the eBusd component."""
     conf = config[DOMAIN]
+    host = conf[CONF_HOST]
     name = conf[CONF_NAME]
     circuit = conf[CONF_CIRCUIT]
     monitored_conditions = conf.get(CONF_MONITORED_CONDITIONS)
-    server_address = (conf.get(CONF_HOST), conf.get(CONF_PORT))
+    circuitmap = {}
+    hass.data[DOMAIN] = data = Data(host, port, name, circuit, monitored_conditions, circuitmap)
 
     try:
-        _LOGGER.debug("Ebusd integration setup started")
+        _LOGGER.debug("setup started")
+        await data.async_connect()
 
-        ebusdpy.init(server_address)
-        hass.data[DOMAIN] = EbusdData(server_address, circuit)
+#         sensor_config = {
+#             CONF_MONITORED_CONDITIONS: monitored_conditions,
+#             "client_name": name,
+#             "sensor_types": SENSOR_TYPES[circuit],
+#         }
+#         load_platform(hass, "sensor", DOMAIN, sensor_config, config)
 
-        sensor_config = {
-            CONF_MONITORED_CONDITIONS: monitored_conditions,
-            "client_name": name,
-            "sensor_types": SENSOR_TYPES[circuit],
-        }
-        load_platform(hass, "sensor", DOMAIN, sensor_config, config)
+        # TODO
+        # hass.services.register(DOMAIN, SERVICE_EBUSD_WRITE, data.write)
 
-        hass.services.register(DOMAIN, SERVICE_EBUSD_WRITE, hass.data[DOMAIN].write)
-
-        _LOGGER.debug("Ebusd integration setup completed")
+        _LOGGER.debug("setup completed")
         return True
     except (socket.timeout, socket.error):
         return False
 
 
-class EbusdData:
-    """Get the latest data from Ebusd."""
+class Data:
 
-    def __init__(self, address, circuit):
-        """Initialize the data object."""
-        self._circuit = circuit
-        self._address = address
-        self.value = {}
+    """Data Handler."""
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self, name, stype):
-        """Call the Ebusd API to update the data."""
-        try:
-            _LOGGER.debug("Opening socket to ebusd %s", name)
-            command_result = ebusdpy.read(
-                self._address, self._circuit, name, stype, CACHE_TTL
-            )
-            if command_result is not None:
-                if "ERR:" in command_result:
-                    _LOGGER.warning(command_result)
-                else:
-                    self.value[name] = command_result
-        except RuntimeError as err:
-            _LOGGER.error(err)
-            raise RuntimeError(err)
+    def __init__(self, host, port, name, circuit, monitored_conditions, circuitmap):
+        self.host = host
+        self.port = port
+        self.name = name
+        self.circuit = circuit
+        self.monitored_conditions = monitored_conditions
+        self.circuitmap = circuitmap
+        self.connection = ebus.Connection(host, port)
 
-    def write(self, call):
+    async def async_connect(self):
+        await self.connection.connect()
+
+    #     @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    #     def update(self, name, stype):
+    #         """Call the Ebusd API to update the data."""
+    #         try:
+    #             _LOGGER.debug("Opening socket to ebusd %s", name)
+    #             command_result = ebusdpy.read(
+    #                 self._address, self._circuit, name, stype, CACHE_TTL
+    #             )
+    #             if command_result is not None:
+    #                 if "ERR:" in command_result:
+    #                     _LOGGER.warning(command_result)
+    #                 else:
+    #                     self.value[name] = command_result
+    #         except RuntimeError as err:
+    #             _LOGGER.error(err)
+    #             raise RuntimeError(err)
+
+    async def async_write(self, call):
         """Call write methon on ebusd."""
+        circuit = call.data.get("circuit", self.circuit)
         name = call.data.get("name")
         value = call.data.get("value")
 
         try:
-            _LOGGER.debug("Opening socket to ebusd %s", name)
-            command_result = ebusdpy.write(self._address, self._circuit, name, value)
-            if command_result is not None:
-                if "done" not in command_result:
-                    _LOGGER.warning("Write command failed: %s", name)
+            await self.connection.write(name, circuit, value)
         except RuntimeError as err:
+            _LOGGER.error(f"ebusd_write(name={name}, circuit={circuit}, value={value}) FAILED")
             _LOGGER.error(err)
