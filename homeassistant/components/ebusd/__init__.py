20ecc83ffb3c1ae7fa35e7fa4a67b6d78ff78a0f
c0fec0de <c0fec0de@gmail.com>
2019-12-29 10:24:15 +0100

    refactor

--- homeassistant/components/ebusd/__init__.py
+++ homeassistant/components/ebusd/__init__.py
@@ -1,11 +1,7 @@
 """Support for Ebusd daemon for communication with eBUS heating systems."""
-import asyncio
-import collections
 import logging
 import socket
-import time
 
-import ebus
 import voluptuous as vol
 
 from homeassistant.const import (
@@ -28,21 +24,12 @@ from .const import (
     DEFAULT_TIMEOUT,
     DOMAIN,
 )
-
-WAIT_MAX = 60
+from .data import Data
 
 _LOGGER = logging.getLogger(__name__)
 
 
-# SERVICE_EBUSD_WRITE = "ebusd_write"
-
-# MONITOR_SCHEMA = vol.Schema(
-#     {
-#         vol.Required(CONF_KNX_EXPOSE_TYPE): cv.string,
-#         vol.Optional(CONF_ENTITY_ID): cv.entity_id,
-#         vol.Required(CONF_KNX_EXPOSE_ADDRESS): cv.string,
-#     }
-# )
+SERVICE_EBUSD_WRITE = "ebusd_write"
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -88,230 +75,17 @@ async def async_setup(hass, config):
     if circuit:
         circuitmap[circuit] = conf[CONF_NAME]
     # monitored_conditions = conf.get(CONF_MONITORED_CONDITIONS)
-    hass.data[DOMAIN] = data = Data(host, port, circuitmap, poll_interval, timeout)
+    data = Data(host, port, circuitmap, poll_interval, timeout)
+    hass.data[DOMAIN] = data
     try:
-        _LOGGER.debug("setup() started")
         await data.async_setup()
 
         load_platform(hass, "sensor", DOMAIN, None, config)
 
-        # TODO
         # hass.services.register(DOMAIN, SERVICE_EBUSD_WRITE, data.write)
 
-        _LOGGER.debug("setup() completed")
+        _LOGGER.debug("setup:   completed")
         return True
     except (socket.timeout, socket.error) as err:
         _LOGGER.error(err)
         return False
-
-
-class Data:
-    """Data Handler."""
-
-    def __init__(self, host, port, circuitmap, poll_interval, timeout):
-        """Container."""
-        self.circuitmap = ebus.CircuitMap(circuitmap)
-        self.query = ebus.Connection(host, port)
-        self.writer = ebus.Connection(host, port, autoconnect=True)
-        self.monitor = ebus.Connection(host, port)
-        self.poll_interval = poll_interval
-        self.timeout = timeout
-        self.fields = ebus.Fields()
-        self.units = ebus.UNITS
-        self.decoder = ebus.Decoder(self.fields, self.units)
-        self.monitors = []
-        self.states = {}
-        self.attrs = {}
-        self.available = {}
-        self.lastseen = {}
-        self.observers = collections.defaultdict(list)
-
-    async def async_setup(self):
-        """Connect and start monitoring."""
-        if self.poll_interval:
-            asyncio.ensure_future(self.async_query())
-        asyncio.ensure_future(self.async_monitor())
-
-        self.fields.load()
-        self.reset_states()
-
-    def reset_states(self):
-        """Reset all states."""
-        for circuit in self.circuitmap.iter_circuits():
-            for field in self.fields.get(circuit):
-                circuitfield = circuit, field
-                self.monitors.append(circuitfield)
-                self.states[circuitfield] = None
-                self.attrs[circuitfield] = {}
-                self.available[circuitfield] = None
-                self.lastseen[circuitfield] = None
-
-    async def async_monitor(self):
-        """Monitor."""
-        monitor = self.monitor
-        wait = 1
-        while True:
-            try:
-                await monitor.connect()
-                await monitor.start_listening(verbose=True)
-                _LOGGER.info("Monitor: started.")
-                wait = 1
-                while True:
-                    line = await monitor.receive()
-                    _LOGGER.debug(f"Monitor: {line!r}")
-                    try:
-                        for value in self.decoder.decode(line):
-                            await self._update(value)
-                            _LOGGER.info(
-                                f"Monitor: {value.circuit}: {value.field.title}={value.value}"
-                            )
-                    except (ValueError, ebus.decoder.FormatError) as err:
-                        _LOGGER.error(f"Monitor: Decode failed: {err}")
-                    except ebus.decoder.UnknownError as err:
-                        _LOGGER.warn(f"Monitor: Decode failed: {err}")
-            except OSError as err:
-                if wait == WAIT_MAX:
-                    self.reset_states()
-                    for methods in self.observers.values():
-                        for method in methods:
-                            await method()
-                    _LOGGER.error(
-                        f"Monitor: {err}, will retry every {wait} seconds, states reset"
-                    )
-                    wait += 1
-                else:
-                    _LOGGER.error(f"Monitor: {err}, retry in {wait} seconds")
-                    wait = min(2 * wait, WAIT_MAX)
-                await asyncio.sleep(wait)
-            finally:
-                await monitor.disconnect()
-
-    async def async_query(self):
-        """Query."""
-        query = self.query
-        wait = 1
-        while True:
-            try:
-                await query.connect()
-                _LOGGER.info("Query:   started.")
-                await asyncio.sleep(self.poll_interval)
-                wait = 1
-                while True:
-                    for circuitfield in tuple(self.states):
-                        circuit, field = circuitfield
-                        if (self.lastseen[circuitfield] or 0) > (
-                            time.time() - self.timeout
-                        ):  # not outdated
-                            continue
-                        if (
-                            self.available[circuitfield] is False
-                        ):  # skip non-available ones
-                            continue
-                        if (
-                            self.available[circuitfield] and field.status
-                        ):  # skip auto-update
-                            continue
-                        try:
-                            line = await self.query.read(
-                                field.name,
-                                circuit=circuit,
-                                ttl=self.timeout,
-                                verbose=True,
-                            )
-                            _LOGGER.debug(f"Query:   {line!r}")
-                            try:
-                                for value in self.decoder.decode(line):
-                                    await self._update(value)
-                                    _LOGGER.info(
-                                        f"Query:   {value.circuit}: {value.field.title}={value.value}"
-                                    )
-                            except (ValueError, ebus.decoder.FormatError) as err:
-                                self.available[circuitfield] = False
-                                _LOGGER.error(f"Monitor: Decode failed: {err}")
-                            except ebus.decoder.UnknownError as err:
-                                self.available[circuitfield] = False
-                                _LOGGER.warn(f"Monitor: Decode failed: {err}")
-                        except ebus.connection.CommandError:
-                            self.available[circuitfield] = False
-                            _LOGGER.warn(
-                                f"{circuit} {field.name} ({field.title}) not available on this EBUS installation"
-                            )
-                        # slow-down
-                        await asyncio.sleep(self.poll_interval)
-                    idle = self.timeout
-                    for (circuit, field), available in self.available.items():
-                        if field.status:
-                            ago = int(time.time() - self.lastseen[(circuit, field)])
-                            _LOGGER.debug(
-                                f"Query:   {circuit} {field.name} ({field.title}): monitored and seen {ago}s ago"
-                            )
-                        elif available:
-                            ago = int(time.time() - self.lastseen[(circuit, field)])
-                            idle = min(idle, max(self.timeout - ago, 0))
-                            _LOGGER.debug(
-                                f"Query:   {circuit} {field.name} ({field.title}): seen {ago}s ago"
-                            )
-                        else:
-                            _LOGGER.debug(
-                                f"Query:   {circuit} {field.name} ({field.title}): Not available, SKIP"
-                            )
-                    _LOGGER.info(f"Query:   wait for {idle}s")
-                    await asyncio.sleep(idle)
-            except OSError as err:
-                if wait == WAIT_MAX:
-                    _LOGGER.error(f"Query:   {err}, will retry every {wait} seconds")
-                    wait += 1
-                else:
-                    _LOGGER.error(f"Query:   {err}, retry in {wait} seconds")
-                    wait = min(2 * wait, WAIT_MAX)
-                await asyncio.sleep(wait)
-            finally:
-                await query.disconnect()
-
-    def add_observer(self, circuit, field, method):
-        """
-        Add observer.
-
-        `method` is called, whenever self.states[circuit] changes.
-        """
-        self.observers[(circuit, field)].append(method)
-
-    async def _update(self, value):
-        circuitfield = value.circuit, value.field
-        self.states[circuitfield] = value.value
-        self.attrs[circuitfield] = value.attrs
-        self.available[circuitfield] = True
-        self.lastseen[circuitfield] = time.time()
-        for method in self.observers[circuitfield]:
-            await method()
-
-    #     @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    #     def update(self, name, stype):
-    #         """Call the Ebusd API to update the data."""
-    #         try:
-    #             _LOGGER.debug("Opening socket to ebusd %s", name)
-    #             command_result = ebusdpy.read(
-    #                 self._address, self._circuit, name, stype, CACHE_TTL
-    #             )
-    #             if command_result is not None:
-    #                 if "ERR:" in command_result:
-    #                     _LOGGER.warning(command_result)
-    #                 else:
-    #                     self.value[name] = command_result
-    #         except RuntimeError as err:
-    #             _LOGGER.error(err)
-    #             raise RuntimeError(err)
-
-    async def async_write(self, call):
-        """Call write methon on ebusd."""
-        circuit = call.data.get("circuit", self.circuit)
-        name = call.data.get("name")
-        value = call.data.get("value")
-
-        try:
-            await self.writer.write(name, circuit, value)
-        except RuntimeError as err:
-            _LOGGER.error(
-                f"ebusd_write(name={name}, circuit={circuit}, value={value}) FAILED"
-            )
-            _LOGGER.error(err)
--- homeassistant/components/ebusd/sensor.py
+++ homeassistant/components/ebusd/sensor.py
@@ -6,29 +6,18 @@ from homeassistant.helpers.entity import Entity
 
 from .const import DOMAIN
 
-# import homeassistant.util.dt as dt_util
-
-
-# TIME_FRAME1_BEGIN = "time_frame1_begin"
-# TIME_FRAME1_END = "time_frame1_end"
-# TIME_FRAME2_BEGIN = "time_frame2_begin"
-# TIME_FRAME2_END = "time_frame2_end"
-# TIME_FRAME3_BEGIN = "time_frame3_begin"
-# TIME_FRAME3_END = "time_frame3_end"
-
 _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Ebus sensor."""
     data = hass.data[DOMAIN]
-    _LOGGER.debug("setup_platform() started")
 
-    entities = []
+    data.statussensor = StatusSensor(data)
+    entities = [data.statussensor]
     for circuit, field in data.monitors:
         entities.append(GenericSensor(data, circuit, field))
-    add_entities(sorted(entities, key=lambda sensor: sensor.name), True)
-    _LOGGER.debug("setup_platform() done")
+    add_entities(entities, True)
 
 
 class GenericSensor(Entity):
@@ -39,8 +28,8 @@ class GenericSensor(Entity):
         self._data = data
         self._circuit = circuit
         self._field = field
-        humanname = data.circuitmap.get_humanname(circuit)
-        self._name = f"{humanname}: {field.title}" if humanname else field.title
+        hname = data.circuitmap.get_humanname(circuit)
+        self._name = f"{hname}: {field.title}" if hname else field.title
         self._unit = data.units.get(field.unitname)
         self._state = None
         self._attrs = {}
@@ -82,13 +71,56 @@ class GenericSensor(Entity):
         """Return Unit."""
         return self._unit and self._unit.uom
 
+    @property
+    def device_state_attributes(self):
+        """Return the device state attributes."""
+        return self._attrs
+
     def update(self):
         """Update internal state."""
         self._state = self._data.states[(self._circuit, self._field)]
         self._attrs = self._data.attrs[(self._circuit, self._field)]
         self._available = self._data.available[(self._circuit, self._field)]
 
+
+class StatusSensor(Entity):
+    """Status Sensor."""
+
+    def __init__(self, data):
+        """Initialize."""
+        self._data = data
+
+    @property
+    def name(self):
+        """Name of the sensor."""
+        return "EBUS"
+
+    @property
+    def should_poll(self):
+        """Poll is not needed."""
+        return False
+
+    @property
+    def state(self):
+        """State of the sensor."""
+        state = self._data.status.get("signal")
+        state = "ok" if state == "acquired" else state
+        return state
+
+    @property
+    def available(self):
+        """Availablity of the sensor."""
+        return True
+
     @property
     def device_state_attributes(self):
         """Return the device state attributes."""
-        return self._attrs
+        return dict(
+            (k, v)
+            for k, v in self._data.status.items()
+            if not isinstance(v, (dict, list, tuple))
+        )
+
+    def update(self):
+        """Update internal state."""
+        pass
